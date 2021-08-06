23c5bdd5fad6fc2c0cfe00bc8152da3b1920bf2e
Matheus Siqueira Sordi <01matheus11@gmail.com>
2021-08-06 16:57:46 -0300

    Remove unused import

--- vecep/src/components/layout/Content.jsx
+++ vecep/src/components/layout/Content.jsx
@@ -1,13 +1,12 @@
 import './Content.css'
 import React from "react"
 import { Switch, Route } from 'react-router-dom'
-import Home from '../../views/contents/Home'
 
 const Content = props => (
     <main className="Content">
         <Switch>
             <Route exact path="/">
-                <Home />
+                
             </Route>
         </Switch>
     </main>
