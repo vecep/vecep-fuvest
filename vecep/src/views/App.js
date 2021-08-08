3c801f95b5f3bfc670650ef3d214574fe92cb6f6
Matheus Siqueira Sordi <01matheus11@gmail.com>
2021-08-08 15:04:10 -0300

    Add side menu

--- vecep/src/views/App.js
+++ vecep/src/views/App.js
@@ -3,11 +3,13 @@ import React from "react"
 import { BrowserRouter as Router } from 'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import SideMenu from '../components/layout/SideMenu'
 
 const App = props => (
     <div className="App">
         <Router>
             <Menu />
+            <SideMenu />
             <Content />
         </Router>
     </div>
