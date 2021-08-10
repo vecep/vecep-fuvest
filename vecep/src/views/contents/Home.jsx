71b3b3177a2689e64e38a59a9ea39cf4970a87da
Victor Yodono <yodono.victor@gmail.com>
2021-08-10 02:15:33 -0300

    Home page structure

--- vecep/src/views/contents/Home.jsx
+++ vecep/src/views/contents/Home.jsx
@@ -1,22 +1,103 @@
-import React from "react"
-import logo from "../../images/blah.png"
-import Button from '@material-ui/core/Button';
-import { Batatinha, Image } from "./style"
+import React from 'react'
+import logo from '../../images/blah.png'
+import { Filter, HomeContainer, Practice, Results, Side, StyledButton } from './styles'
+import { Skeleton } from '@material-ui/lab'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import DiscordIcon from '../../images/discord-icon.svg';
+import ScrollButton from '../../components/utils/scrollButton';
 
-const Home = props => (
-    <Batatinha>
-        <div className="home-info">
-            <div className="home-info-text">
-                <h3>Afinal... quem somos nós?!</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur neque tempus, elementum leo ut, gravida eros. Phasellus malesuada arcu elit, id hendrerit neque convallis sed. Suspendisse condimentum lacinia nisl congue posuere. Nullam elementum leo at rutrum imperdiet. Nam varius lacus nunc.</p>
-                <p>Quisque feugiat libero et hendrerit auctor. Fusce venenatis nec lacus id lacinia. Phasellus tempor finibus mauris id feugiat. Praesent ac lectus augue. Aenean non augue purus. Proin ornare vestibulum nisl, vel rhoncus risus. Integer porttitor tincidunt semper. In ante erat, malesuada id enim quis, lacinia volutpat velit. Phasellus ut aliquet ante.</p>
-            </div>
-            <Image>
-                <img src={logo} alt='' className="home-info-image" />
-            </Image>
+const Home = () => {
+
+  const renderPage = () => (
+    <HomeContainer>
+      <div className="home-intro">
+        <div className="text">
+          <span>Afinal... quem somos nós?!</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur neque tempus,
+            elementum leo ut, gravida eros. Phasellus malesuada arcu elit, id hendrerit neque
+            convallis sed. Suspendisse condimentum lacinia nisl congue posuere. Nullam elementum leo
+            at rutrum imperdiet. Nam varius lacus nunc.
+          </p>
+          <p>
+            Quisque feugiat libero et hendrerit auctor. Fusce venenatis nec lacus id lacinia.
+            Phasellus tempor finibus mauris id feugiat. Praesent ac lectus augue. Aenean non augue
+            purus. Proin ornare vestibulum nisl, vel rhoncus risus. Integer porttitor tincidunt
+            semper. In ante erat, malesuada id enim quis, lacinia volutpat velit. Phasellus ut aliquet
+            ante.
+          </p>
         </div>
-        <Button variant="contained" color="primary">oi</Button>
-    </Batatinha>
-)
+        <div className="image">
+          <img src={logo} alt="" className="home-info-image" />
+        </div>
+      </div>
+      <StyledButton>Começe Já!</StyledButton>
 
-export default Home
+      <div className="home-statistics">
+        <span>+40<br />provas</span>
+        <span>+3600<br />questões</span>
+        <span>+117.000<br />candidatos</span>
+      </div>
+
+      <div className="feature-grid">
+          <Side>
+              <span className="title">Separamos os conteúdos bonitinhos, pra você...</span>
+              <Skeleton variant="rect" className="side"/>
+          </Side>
+
+          <Results>
+              <span className="title">Veja seus resultados</span>
+              <Skeleton variant="rect" className="results"/>
+          </Results>
+          <Practice>
+              <span className="title">Pratique, pratique, pratique!</span>
+              <Skeleton variant="rect" className="practice" />
+          </Practice>
+
+          <Filter>
+              <span className="title">Filtre para achar o que você precisa!</span>
+              <Skeleton variant="rect" className="filter" />
+          </Filter>
+      </div>
+
+      <div className="home-footer">
+        <div className="footer-info">
+          <div className="contact">
+            <span>Converse com a gente!</span>
+            <span>(19) 9 9999-9999</span>
+            <span><b>vecep.fuvest@gmail.com</b></span>
+          </div>
+
+          <div className="about">
+            <span>Sobre a Fuvest</span>
+            <span>Site do vestibular</span>
+            <span>Portal da USP</span>
+          </div>
+        </div>
+
+        <div className="social-media">
+          <div>
+            <span>Acesse nossas redes</span>
+            <div>
+              <FacebookIcon />
+              <img src={DiscordIcon} alt=""/>
+              <TwitterIcon />
+              <InstagramIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </HomeContainer>
+  )
+
+  return (
+    <>
+      {renderPage()}
+      <ScrollButton />
+    </>
+  );
+}
+
+export default Home;
---
+++ vecep/src/views/contents/styles.js
@@ -0,0 +1,235 @@
+import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: static;
+  box-sizing: border-box;
+
+  & > div {
+    box-sizing: border-box;
+    padding: 0 100px;
+  }
+
+  .home-intro {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-top: 70px;
+    padding-bottom: 50px;
+
+    .text {
+      width: 50%;
+      font-size: 1.5em;
+
+      span {
+        display: block;
+        font-size: 1.2em;
+        margin-block-start: 0.83em;
+        margin-block-end: 0.83em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+      }
+    }
+
+    .image {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50%;
+
+      img {
+        transform: scaleX(-1);
+      }
+      
+      @media only screen and (max-width: 600px) {
+        img {
+          height: 500px;
+        }
+      }
+
+      @media only screen and (max-width: 1280px) {
+        img {
+          height: 500px;
+        }
+      }
+    }
+  }
+
+  .home-statistics {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 275px;
+    margin: 50px 0 130px 0;
+    padding: 70px 100px 50px 100px;
+    background-color: #282727;
+    color: white;
+    
+    span {
+      text-align: center;
+      font-size: 3em;
+    }
+  }
+
+  .feature-grid {
+    display: grid;
+    justify-items: stretch;
+    align-items: stretch;
+    grid-template-rows: 17em 17em;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-areas:
+      'side results practice'
+      'side filter filter';
+    gap: 5em;
+    margin-bottom: 100px;
+
+    .title {
+      display: block;
+      font-size: 1.2em;
+      margin-block-end: 0.83em;
+      font-weight: bold;
+    }
+  }
+
+  .home-footer {
+    display: flex;
+    flex-direction: row;
+    background-color: #282727;
+    width: 100%;
+    height: 275px;
+    margin-top: 50px;
+    color: white;
+
+    .footer-info {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      width: 50%;
+    }
+
+    .social-media {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      width: 50%;
+      box-sizing: border-box;
+
+      div > div:last-of-type {
+        display: inline-block;
+        
+        svg, img {
+          font-size: 2rem;
+          margin: 1.5rem 1.5rem 0 0;
+        }
+      }
+    }
+
+    & > div {
+      & > div {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        font-size: 1.125rem;
+      }
+
+      span:first-child {
+        font-size: 1.5em;
+      }
+
+      span:nth-child(2) {
+        margin-top: 1.5rem;
+      }
+    }
+    
+    span {
+      text-align: center;
+      font-size: 1em;
+    }
+  }
+`;
+
+const Side = styled.div`
+  grid-row: side-start / side-end;
+	grid-column: side-start / side-end;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .side {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Results = styled.div`
+  grid-row: results-start / results-end;
+	grid-column: results-start / results-end;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .results {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Practice = styled.div`
+  grid-row: practice-start / practice-end;
+	grid-column: practice-start / practice-end;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .practice {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Filter = styled.div`
+  grid-row: filter-start / filter-end;
+	grid-column: filter-start / filter-end;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .filter {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const StyledButton = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #21CBF3 30%, #48A7FF 90%)',
+    borderRadius: 2,
+    color: 'white',
+    height: 37,
+    width: 205,
+    padding: '0 30px',
+    boxSizing: 'border-box',
+    boxShadow: '0 3px 5px 2px rgba(27, 68, 105, .3)',
+    fontWeight: 'bold',
+
+    '&:hover': {
+      background: 'linear-gradient(45deg, #48A7FF 30%, #21CBF3 90%)',
+    }
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Button);
+
+export { HomeContainer, StyledButton, Side, Results, Practice, Filter };
