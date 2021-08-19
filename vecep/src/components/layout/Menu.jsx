4aabe0a6352797827b89f23210cf43a81ab769a4
Victor Yodono <yodono.victor@gmail.com>
2021-08-18 23:38:06 -0300

    Minor changes

--- vecep/src/components/layout/Menu.jsx
+++ vecep/src/components/layout/Menu.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => (
-  <aside className="Menu">
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">VECEP</Link>
-        </li>
-      </ul>
-    </nav>
-  </aside>
+	<aside className="Menu">
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">VECEP</Link>
+				</li>
+			</ul>
+		</nav>
+	</aside>
 );
 export default Menu;
