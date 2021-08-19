e84bc3413309f01af240e05423f71ab5e264ed14
Victor Yodono <yodono.victor@gmail.com>
2021-08-18 23:34:51 -0300

    Adjusting SideMenu rendering condition

--- vecep/src/views/App.js
+++ vecep/src/views/App.js
@@ -1,20 +1,22 @@
-import './App.css'
-import React from "react"
-import { BrowserRouter as Router } from 'react-router-dom'
-import Menu from '../components/layout/Menu'
-import Content from '../components/layout/Content'
-import SideMenu from '../components/layout/SideMenu'
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+import SideMenu from '../components/layout/SideMenu';
 
-const App = props => (
-    <div className="App">
-        <Router>
-            <Menu />
-            <div className="side-menu-content"> 
-                <SideMenu />
-                <Content />
-            </div>
-        </Router>
-    </div>
-)
+const App = () => (
+	<div className="App">
+		<Router>
+			<Menu />
+			<div className="side-menu-content">
+				<div className="side-menu-content"> 
+					<Route path={['/exercicios', '/provas', '/resultados']} component={SideMenu} />
+					<Content />
+				</div>
+			</div>
+		</Router>
+	</div>
+);
 
 export default App;
