0b1a77a364d1782f82761bcd17ddba29c39788a7
Victor Yodono <yodono.victor@gmail.com>
2021-08-21 18:59:36 -0300

    Refactoring styles & add answer color feedback

--- vecep/src/components/card/Card.jsx
+++ vecep/src/components/card/Card.jsx
@@ -1,87 +1,92 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import Radio from '@material-ui/core/Radio';
 import { withStyles } from '@material-ui/core/styles';
-import './Card.css';
+import CardContainer from './style';
 
 const OptionRadio = withStyles({
-    root: {
-        color: 'black',
-        padding: 0,
+	root: {
+		color: 'black',
+		padding: 0,
         
-        '&$checked': {
-        color: '#48A7FF',
-        },
-    },
-    checked: {},
+		'&$checked': {
+			color: '#48A7FF',
+		},
+	},
+	checked: {},
 })((props) => <Radio color="default" {...props} />);
 
 const Card = ({ question, options, references, test }) => {
-    const [selectedValue, setSelectedValue] = useState();
+	const [selectedValue, setSelectedValue] = useState();
+	const [answered, setAnswered] = useState(false);
+	const correctAnswer = options.find(o => o.correct_answer === true);
 
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
+	const handleChange = (event) => {
+		setSelectedValue(event.target.value);
+	};
 
-    const handleAnswer = () => {
-        const correctAnswer = options.find(o => o.correct_answer === true);
+	const handleAnswers = () => setAnswered(true);
 
-        const message = selectedValue === correctAnswer.id.toString() ? 'acertou' : 'errou';
-        window.alert(message);
-    }
+	const renderOptions = () => (
+		options.map(q => {
+			const isWrong = selectedValue != correctAnswer.id.toString() && selectedValue === q.id.toString() && 'wrong';
+			const showAnswer = q.id === correctAnswer.id ? 'correct' : isWrong;
 
-    const renderOptions = () => (
-        options.map(q => 
-            <div className="options-container" key={q.id}>
-                <OptionRadio
-                    checked={selectedValue === q.id.toString()}
-                    onChange={handleChange}
-                    value={q.id}
-                />
-                <span className="question">{q.text}</span>
-            </div>
-        )
-    );
+			return(
+				<div className="options-container" key={q.id}>
+					<OptionRadio
+						checked={selectedValue === q.id.toString()}
+						onChange={handleChange}
+						value={q.id}
+						disabled={answered}
+					/>
+					<span className={answered && showAnswer}>{q.text}</span>
+				</div>
+			);
+		})
+	);
 
-    const renderReferences = () => (
-        references.map(r => {
-            const renderImage = r.image_path && <img src={r.image_path} alt="" />;
-            const renderText = r.text && <span>{r.text}</span>;
-            const renderSource = r.source && <span className="source">{r.source}</span>;
-            const renderAuthor = r.author && <span className="author">{r.author}</span>;
+	const renderReferences = () => (
+		references.map(r => {
+			const renderImage = r.image_path && <img src={r.image_path} alt="" />;
+			const renderText = r.text && <span>{r.text}</span>;
+			const renderSource = r.source && <span className="source">{r.source}</span>;
+			const renderAuthor = r.author && <span className="author">{r.author}</span>;
 
-            return (    
-                <div className="reference" key={r.id}>
-                    {renderImage}
-                    {renderText}
-                    {renderSource}
-                    {renderAuthor}
-                    <hr />
-                </div>
-            )
-        })
-    );
+			return (    
+				<div className="reference" key={r.id}>
+					{renderImage}
+					{renderText}
+					{renderSource}
+					{renderAuthor}
+					<hr />
+				</div>
+			);
+		})
+	);
 
-    return (
-        <div className="question-container">
-            <span>{test.year}</span>
-            <div className="card-container">
-                <div className="card-header">
-                    <div className="card-content">
-                        <div className="question-text">
-                            <p className="question-instruction">{question.text}</p>              
-                            {renderOptions()}
-                        </div>
-                        <div className="question-ref">
-                            {renderReferences()}
-                        </div>
-                    </div>
-                </div>
-                <div className="card-footer">
-                    <button className="answer" onClick={handleAnswer}>Responder</button>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<CardContainer>
+			<div className="card-info">
+				<span>{question.subject} - {question.genre}</span>
+				<span>{test.year}</span>
+			</div>
+			<div className="card-header">
+				<div className="card-content">
+					<div className="question-text">
+						<p className="question-instruction">{question.text}</p>              
+						{renderOptions()}
+					</div>
+					<div className="question-ref">
+						{renderReferences()}
+					</div>
+				</div>
+			</div>
+			<div className="card-footer">
+				<button className="answer" onClick={handleAnswers} disabled={answered}>Responder</button>
+			</div>
+		</CardContainer>
+	);
 };
 
 export default Card;
--- vecep/src/components/card/style.js
+++ vecep/src/components/card/style.js
@@ -1,21 +1,33 @@
-.question-container {
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+display: flex;
+flex-direction: column;
+font-size: 2em;
+color: black;
+margin-top: 60px;
+
+.card-info {
   display: flex;
-  flex-direction: column;
-  text-align: right;
-  font-size: 2em;
-  color: black;
-  margin-top: 60px;
+  justify-content: space-between;
+
+  span {
+    margin: 0 20px 5px 15px;
+    font-size: 1.2rem;
+
+    &:last-child {
+      font-weight: bold;  
+      font-size: 1.5rem;
+    }
+  }
 }
 
-.question-container > span {
-  margin-right: 20px;
-  margin-bottom: 5px;
-  font-weight: bold;
-  font-size: 1.5rem;
+.correct {
+  color: green;
 }
 
-.card-container {
-  margin-left: 5vw;
+.wrong {
+  color: red;
 }
 
 .card-header {
@@ -79,16 +91,15 @@
   display: flex;
   align-items: center;
   margin-bottom: 15px;
+
+  span {
+    font-size: 1rem;
+    margin-left: 10px;
+  }
 }
 
 .options-container svg {
   width: 1rem;
-}
-
-.question {
-  color: black;
-  font-size: 1rem;
-  margin-left: 10px;
 }
 
 .question-ref { 
@@ -148,3 +159,6 @@
 .card-content::-webkit-scrollbar-thumb:hover {
   background: grey; 
 }
+`;
+
+export default CardContainer;
