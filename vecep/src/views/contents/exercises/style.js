417a3f8a3ff72bec7fdbbf7de5fa777c92786281
Victor Yodono <yodono.victor@gmail.com>
2021-08-21 18:58:09 -0300

    Refactoring styles

---
+++ vecep/src/views/contents/exercises/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ExercisesContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-bottom: 50px;
+`;
+
+export default ExercisesContainer;
