bbae11cca3c91cb2ef294b68cd8b676c5d16a1c9
Victor Yodono <yodono.victor@gmail.com>
2021-09-05 14:01:53 -0300

    Test CRUD

---
+++ server/src/modules/test/controller/TestController.js
@@ -0,0 +1,50 @@
+const db = require('../../../database/connection');
+
+exports.post = (req, res) => {
+  const test = req.body;
+
+  db.insert(test).table("test")
+    .then(() =>
+      res.status(200).json({ test })
+    )
+    .catch((err) => console.log(err));
+}
+
+exports.get = (_, res) => {
+  db.select('*').table('test')
+    .then((data) => {
+      res.status(200).json({ data })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.getOneById = (req, res) => {
+  const { id } = req.params;
+
+  db.select('*').table('test').where({ id })
+    .then(([data]) => {
+      res.status(200).json({ data })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.put = (req, res) => {
+  const { year, stage } = req.body;
+  const { id } = req.params;
+
+  db.update({ id, year, stage }).table('test').where({ id })
+    .then(() => {
+      res.status(200).json({ message: 'Prova atualizada com sucesso!' })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.delete = (req, res) => {
+  const { id } = req.params;
+
+  db.delete().table("test").where({ id })
+    .then(() => {
+      res.status(204).json({ message: "Prova excluída com sucesso!" })
+    })
+    .catch((err) => console.log(err));
+}
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 // const QuestionController = require('../modules/question/controller/QuestionController');
-const ReferenceController = require('../modules/reference/controller/ReferenceController')
+const ReferenceController = require('../modules/reference/controller/ReferenceController');
+const TestController = require('../modules/test/controller/TestController');
 
 // router.get('/api/getAllQuestions', QuestionController.getAllQuestions);
 // router.post('/api/createQuestion', QuestionController.createQuestion);
@@ -12,5 +13,11 @@ router.get('/api/references', ReferenceController.get);
 router.get('/api/reference/:id', ReferenceController.getOneById);
 router.put('/api/reference/:id', ReferenceController.put);
 router.delete('/api/reference/:id', ReferenceController.delete);
+
+router.post('/api/test', TestController.post);
+router.get('/api/tests', TestController.get);
+router.get('/api/test/:id', TestController.getOneById);
+router.put('/api/test/:id', TestController.put);
+router.delete('/api/test/:id', TestController.delete);
 
 module.exports = router;
