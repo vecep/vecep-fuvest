9f3af4be075895e9e81760bbae3509d89555bd45
Victor Yodono <yodono.victor@gmail.com>
2021-09-05 13:48:43 -0300

    Reference CRUD

---
+++ server/src/modules/reference/controller/ReferenceController.js
@@ -0,0 +1,50 @@
+const db = require('../../../database/connection');
+
+exports.post = (req, res) => {
+  const reference = req.body;
+
+  db.insert(reference).table("reference")
+    .then(() =>
+      res.status(200).json({ reference })
+    )
+    .catch((err) => console.log(err));
+}
+
+exports.get = (_, res) => {
+  db.select('*').table('reference')
+    .then((data) => {
+      res.status(200).json({ data })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.getOneById = (req, res) => {
+  const { id } = req.params;
+
+  db.select('*').table('reference').where({ id })
+    .then(([data]) => {
+      res.status(200).json({ data })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.put = (req, res) => {
+  const { text, author, source, image_path } = req.body;
+  const { id } = req.params;
+
+  db.update({ id, text, author, source, image_path }).table('reference').where({ id })
+    .then(() => {
+      res.status(200).json({ message: 'Referência atualizada com sucesso!' })
+    })
+    .catch((err) => console.log(err));
+}
+
+exports.delete = (req, res) => {
+  const { id } = req.params;
+
+  db.delete().table("reference").where({ id })
+    .then(() => {
+      res.status(204).json({ message: "Referência excluída com sucesso!" })
+    })
+    .catch((err) => console.log(err));
+}
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,4 +1,16 @@
 const express = require('express');
+
 const router = express.Router();
+// const QuestionController = require('../modules/question/controller/QuestionController');
+const ReferenceController = require('../modules/reference/controller/ReferenceController')
+
+// router.get('/api/getAllQuestions', QuestionController.getAllQuestions);
+// router.post('/api/createQuestion', QuestionController.createQuestion);
+
+router.post('/api/reference', ReferenceController.post);
+router.get('/api/references', ReferenceController.get);
+router.get('/api/reference/:id', ReferenceController.getOneById);
+router.put('/api/reference/:id', ReferenceController.put);
+router.delete('/api/reference/:id', ReferenceController.delete);
 
 module.exports = router;
