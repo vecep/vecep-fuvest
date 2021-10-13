db507eea3b3ad5b38cf47688b8b3ecdda99b389c
Victor Yodono <yodono.victor@gmail.com>
2021-10-13 20:16:21 -0300

    Refactor and add image display on options

--- client/src/components/card/components/cardQuestion/styles.js
+++ client/src/components/card/components/cardQuestion/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Radio from '@material-ui/core/Radio';
 import { withStyles } from '@material-ui/core/styles';
+import { get } from 'lodash';
 
 const QuestionContainer = styled.div`
 	overflow-wrap: break-word;
@@ -30,6 +31,7 @@ const Option = withStyles({
 	root: {
 		color: 'black',
 		padding: 0,
+		marginRight: 7,
 		transition: 'color .5s',
 
 		'&$checked': {
@@ -45,4 +47,17 @@ const Label = styled.span`
 	color: ${(props) => props.answerColor};
 `;
 
-export { QuestionContainer, Text, OptionsContainer, Option, Label };
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: auto auto auto;
+	grid-template-rows: auto auto auto auto auto;
+	grid-auto-flow: ${({ displayImageGrid }) => displayImageGrid ? 'row' : 'column'};
+`;
+
+const ImageLabel = styled.div`
+	width: 75%;
+ 	height: auto;
+	box-shadow: 0 0 15px 0 ${(props) => get(props, 'answerColor', 'transparent')};
+`;
+
+export { QuestionContainer, Text, OptionsContainer, Option, Label, Grid, ImageLabel };
