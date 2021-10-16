b74180d1c43488922414ac9d5bc0aab7a21ede0e
Victor Yodono <yodono.victor@gmail.com>
2021-10-16 17:39:42 -0300

    Refactor in order to filter by query params

--- client/src/components/layout/content/index.jsx
+++ client/src/components/layout/content/index.jsx
@@ -1,35 +1,57 @@
-import ContentContainer from './styles.js';
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import Exercises from '../../../views/contents/exercises/';
 import Home from '../../../views/contents/home/';
 import Tests from '../../../views/contents/tests/';
 import Admin from '../../../views/contents/admin/';
 import NotFound from '../../../views/contents/not-found/';
+import ContentContainer from './styles.js';
+import { AppContext } from '../../../contexts/store';
 
-const Content = () => (
-	<ContentContainer>
-		<Switch>
-			<Route exact path="/">
-				<Redirect to="/home" />
-			</Route>
-			<Route exact path="/home">
-				<Home />
-			</Route>
-			<Route path="/exercicios">
-				<Exercises />
-			</Route>
-			<Route path="/provas">
-				<Tests />
-			</Route>
-			<Route path="/admin">
-				<Admin />
-			</Route>
-			<Route path="*">
-				<NotFound />
-			</Route>
-		</Switch>
-	</ContentContainer>
-);
+const Content = () => {
+	const history = useHistory();
+	const { params } = useContext(AppContext);
+
+	useEffect(async () => {
+		if (params) {
+			const newParams = new URLSearchParams();
+
+			for (const [key, value] of Object.entries(params)) {
+				if (value) {
+					newParams.append(key, value);
+				} else {
+					newParams.delete(key);
+				}
+			}
+
+			history.push( { search: newParams.toString() });
+		}
+	}, [params, history]);
+
+	return (
+		<ContentContainer>
+			<Switch>
+				<Route exact path="/">
+					<Redirect to="/home" />
+				</Route>
+				<Route exact path="/home">
+					<Home />
+				</Route>
+				<Route path="/exercicios">
+					<Exercises />
+				</Route>
+				<Route path="/provas">
+					<Tests />
+				</Route>
+				<Route path="/admin">
+					<Admin />
+				</Route>
+				<Route path="*">
+					<NotFound />
+				</Route>
+			</Switch>
+		</ContentContainer>
+	);
+};
 
 export default Content;
--- client/src/views/contents/exercises/index.jsx
+++ client/src/views/contents/exercises/index.jsx
@@ -1,65 +1,53 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
+import useQuery from '../../../hooks/useQuery';
 import Card from '../../../components/card';
 import TextField from '@material-ui/core/TextField';
 import { ExercisesContainer, Header, FilterContainer, SubjectTitle } from './styles';
 import { StyledAutocomplete } from '../../../components/utils/autocomplete/style';
 import { AppContext } from '../../../contexts/store';
 import Axios from 'axios';
+import { toTitleCase } from '../../../utils/toTitleCase';
 
 const Exercises = () => {
 	const location = useLocation();
-
-	const { contextYears } = useContext(AppContext);
+	const query = useQuery();
+	const { contextYears, setParams } = useContext(AppContext);
 
 	const [exercises, setExercises] = useState([]);
-	const [selectedYear, setSelectedYear] = useState();
-	const [selectedTopic, setSelectedTopic] = useState();
 	const [filteredTopics, setFilteredTopics] = useState([]);
-	const [subjectTitle, setSubjectTitle] = useState();
-	const [filteredExercises, setFilteredExercises] = useState([]);
+	const [subjectTitle, setSubjectTitle] = useState('');
 
 	useEffect(async () => {
-		const { data: exercises } = await Axios.get('http://localhost:3001/api/exercises', {
-			params: { subject: location.pathname.split('/').pop() }
-		});
-
-		setExercises(exercises);
-		setFilteredExercises(exercises);
+		await getExercises();
 	}, [location]);
 
 	useEffect(() => {
-		const filteredExercises = exercises.filter((exercise) => {
-			const { test, question } = exercise;
-
-			const filterByTopic = selectedTopic
-				? question.topic.toLowerCase() === selectedTopic.toLowerCase()
-				: true;
-			const filterByYear = selectedYear ? test.year === selectedYear : true;
-
-			return filterByTopic && filterByYear;
-		});
-
-		setFilteredExercises(filteredExercises);
-	}, [selectedYear, selectedTopic]);
-
-	useEffect(() => {
-		const filteredSubjects = [...new Set(filteredExercises.map((fe) => fe.question.subject))];
-		const subjectTitle = filteredSubjects.some((c) => c !== filteredSubjects[0])
-			? null
-			: filteredSubjects.shift();
-
-		const sameSubjectExercises = exercises.filter((e) =>
-			subjectTitle ? e.question.subject === subjectTitle : true
-		);
-		const filteredTopics = [...new Set(sameSubjectExercises.map((e) => e.question.topic))];
+		const subjectTitle = query.get('subject') || 'Geral';
+		const filteredTopics = [...new Set(exercises.map((e) => e.question.topic))];
 
 		setFilteredTopics(filteredTopics);
 		setSubjectTitle(subjectTitle);
-	}, [filteredExercises]);
+	}, [exercises]);
+
+	const getExercises = async () => {
+		try {
+			const { data } = await Axios.get('http://localhost:3001/api/exercises', {
+				params: {
+					subject: query.get('subject'),
+					topic: query.get('topic'),
+					year: query.get('year')
+				}
+			});
+
+			setExercises(data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
 	const renderCards = () =>
-		filteredExercises.map((exercise) => {
+		exercises.map((exercise) => {
 			const { test, question, options, references } = exercise;
 
 			return (
@@ -76,13 +64,13 @@ const Exercises = () => {
 	return (
 		<ExercisesContainer>
 			<Header>
-				<SubjectTitle>{subjectTitle || 'Geral'}</SubjectTitle>
+				<SubjectTitle>{toTitleCase(subjectTitle)}</SubjectTitle>
 
 				<FilterContainer>
 					<StyledAutocomplete
 						className="topic-filter"
 						options={filteredTopics}
-						onChange={(_, value) => setSelectedTopic(value)}
+						onChange={(_, value) => setParams({ topic: value })}
 						renderInput={(params) => (
 							<TextField {...params} label="Pesquise por tema..." variant="outlined" />
 						)}
@@ -91,7 +79,7 @@ const Exercises = () => {
 						className="year-filter"
 						options={contextYears}
 						getOptionLabel={(o) => o.toString()}
-						onChange={(_, value) => setSelectedYear(value)}
+						onChange={(_, value) => setParams({ year: value })}
 						renderInput={(params) => <TextField {...params} label="Ano" variant="outlined" />}
 					/>
 				</FilterContainer>
