b20fd92eb251fbeb21c7ade28a55daf313c50c36
Victor Yodono <yodono.victor@gmail.com>
2021-10-20 19:59:52 -0300

    Add authentication service on client

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,33 @@
+import Axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/auth/';
+
+class AuthService {
+	async login(username, password) {
+		const { data } = await Axios.post(API_URL + 'signin', {
+			username,
+			password
+		});
+		data.accessToken && localStorage.setItem('user', JSON.stringify(data));
+
+		return data;
+	}
+
+	logout() {
+		localStorage.removeItem('user');
+	}
+
+	register(username, email, password) {
+		return Axios.post(API_URL + 'signup', {
+			username,
+			email,
+			password
+		});
+	}
+
+	getCurrentUser() {
+		return JSON.parse(localStorage.getItem('user'));
+	}
+}
+
+export default new AuthService();
