d8f44af63ffea76bc660eb82e37e1808c730cb07
Victor Yodono <yodono.victor@gmail.com>
2021-10-22 00:05:21 -0300

    Adapt "get started" button on home based on login

--- client/src/views/contents/home/components/homeIntro/index.jsx
+++ client/src/views/contents/home/components/homeIntro/index.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import HumaaanPNG from '../../../../../images/humaaan.png';
 import { Link } from 'react-router-dom';
 import { Container, Intro, Text, Title, Image, StyledButton } from './styles';
+import { AuthContext } from '../../../../../contexts/AuthContext';
 
-const HomeIntro = () => (
-	<Container>
-		<Intro>
-			<Text>
-				<Title>Afinal... o que fazemos?!</Title>
-				<p>
+const HomeIntro = () => {
+	const { isLoggedIn } = useContext(AuthContext);
+
+	return (
+		<Container>
+			<Intro>
+				<Text>
+					<Title>Afinal... o que fazemos?!</Title>
+					<p>
 					Somos uma ferramenta que auxilia você, vestibulando da Fuvest, a colocar em prática os
 					conteúdos estudados, por meio de questões específicas.
-				</p>
-				<p>
+					</p>
+					<p>
 					Nossa missão é facilitar ao máximo sua jornada em direção à aprovação! Aqui, você pode
 					encontrar tudo relacionado às provas da USP, desde 2010.
-				</p>
-			</Text>
-			<Image src={HumaaanPNG} alt="" />
-		</Intro>
+					</p>
+				</Text>
+				<Image src={HumaaanPNG} alt="" />
+			</Intro>
 
-		<StyledButton>
-			<Link to="/exercicios" draggable={false}>
+			<StyledButton>
+				<Link to={isLoggedIn ? '/exercicios' : '/registrar'} draggable={false}>
 				Comece Já!
-			</Link>
-		</StyledButton>
-	</Container>
-);
+				</Link>
+			</StyledButton>
+		</Container>
+	);
+};
 
 export default HomeIntro;
